20ec9d3814dfba9152301047fa5d62fa47f477ab
Rana adeel <112337492+ranaadeel15911@users.noreply.github.com>
2024-01-11 23:37:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -86,13 +86,13 @@ app.get("/auth/google/callback",passport.authenticate("google",{
     failureRedirect:"https://o-auth-client.vercel.app/login"
 }))
 app.get("/login/sucess",async(req,res)=>{
-console.log(req)
-    if(req.user){
+    // if(req.user){
         res.status(200).json({message:"user Login",user:req.user})
-        return
-    }else{
-        res.status(400).json({message:"Not Authorized"})
-    }
+       
+    // }
+    // else{
+    //     res.status(400).json({message:"Not Authorized"})
+    // }
 })
 app.get("/",(req,resp)=>{
     resp.json("Server Running")
