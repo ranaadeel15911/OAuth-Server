3b29547681e5864df45682ce3370b9ae13c3cc1e
Rana adeel <112337492+ranaadeel15911@users.noreply.github.com>
2024-01-13 20:08:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.get("/auth/google/callback",passport.authenticate("google",{
     successRedirect:"https://o-auth-client.vercel.app/dashboard",
     failureRedirect:"https://o-auth-client.vercel.app/login"
 }))
-app.post("/login/success",async(req,res)=>{
+app.get("/login/sucess",async(req,res)=>{
     if(req.user){
         res.status(200).json({message:"user Login",user:req.user})
     }else{
