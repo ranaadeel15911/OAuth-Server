ad3337fcaadf65778762f8df553b25bb98cf8902
Rana adeel <112337492+ranaadeel15911@users.noreply.github.com>
2024-01-13 15:08:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.get("/auth/google/callback",passport.authenticate("google",{
 }))
 app.get("/login/sucess",async(req,res)=>{
     if(req.user){
-        res.status(200).json({req.user})
+        res.status(200).json({message:"Authorized"})
     }else{
         res.status(400).json({message:"Not Authorized"})
     }
